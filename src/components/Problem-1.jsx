752fc2cc13298d801cd10b2dd5dc2379916f43f4
Mohammad Minhaz <minhazkhan312@gmail.com>
2023-12-13 17:24:20 +0600

    final commit

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,53 +1,176 @@
 import React, {useState} from 'react';
+import status_list from "../data/status_list";
 
 const Problem1 = () => {
+    const [show, setShow] = useState("all");
 
-    const [show, setShow] = useState('all');
+    const [taskName, setTaskName] = useState("");
+    const [taskStatus, setTaskStatus] = useState("");
 
-    const handleClick = (val) =>{
-        setShow(val);
-    }
+    const [taskList, setTaskList] = useState([]);
+    const [filteredTaskList, setFilteredTaskList] = useState([]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (taskName.trim() === "" || taskStatus.trim() === "") {
+            alert("Task name and Task status required!");
+        } else {
+            const validStatus = status_list.find(
+                (ts) => ts.toLowerCase() === taskStatus.toLowerCase()
+            );
+            console.log(validStatus);
+            if (validStatus === undefined) {
+                alert("Task status is not valid!");
+            } else {
+                const exist = taskList.find(
+                    (t) => t.task_name === taskName.trim()
+                );
+                if (exist === undefined) {
+                    const formatedTaskStatus = taskStatus
+                        .trim()
+                        .substring(0, 1)
+                        .toUpperCase()
+                        .concat(taskStatus.substring(1).toLowerCase());
+                    const newTask = {
+                        task_name: taskName.trim(),
+                        task_status: formatedTaskStatus,
+                    };
+                    setTaskList((tl) => [...tl, newTask]);
+                    if (show === "all") {
+                        setFilteredTaskList((prevList) => [
+                            ...prevList,
+                            newTask,
+                        ]);
+                    } else if (show.trim() === taskName.trim().toLowerCase()) {
+                        setFilteredTaskList((prevList) => [
+                            ...prevList,
+                            newTask,
+                        ]);
+                    }
+                    setTaskName("");
+                    setTaskStatus("");
+                } else {
+                    alert(
+                        `Task already exist with status ${exist.task_status}`
+                    );
+                }
+            }
+        }
+    };
+
+    const handleClick = (type) => {
+        setShow(type);
+        if (type.toLowerCase() === "all") {
+            let allActiveTask = [];
+            let allCompletedTask = [];
+            let restOfTheTask = [];
+            taskList.map((task) => {
+                if (task.task_status === "Active") {
+                    allActiveTask.push(task);
+                } else if (task.task_status === "Completed") {
+                    allCompletedTask.push(task);
+                } else {
+                    restOfTheTask.push(task);
+                }
+            });
+            setFilteredTaskList([
+                ...allActiveTask,
+                ...allCompletedTask,
+                ...restOfTheTask,
+            ]);
+        } else {
+            let newList = [];
+            taskList.map((task) => {
+                if (task.task_status.toLowerCase() === type.toLowerCase()) {
+                    newList.push(task);
+                }
+            });
+            setFilteredTaskList(newList);
+        }
+    };
 
     return (
-
         <div className="container">
             <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
+                <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
                 <div className="col-6 ">
                     <form className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                                value={taskName}
+                                onChange={(e) => setTaskName(e.target.value)}
+                            />
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Status"
+                                value={taskStatus}
+                                onChange={(e) => setTaskStatus(e.target.value)}
+                            />
                         </div>
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button
+                                type="submit"
+                                onClick={handleSubmit}
+                                className="btn btn-primary"
+                            >
+                                Submit
+                            </button>
                         </div>
                     </form>
                 </div>
                 <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <ul
+                        className="nav nav-pills mb-3"
+                        id="pills-tab"
+                        role="tablist"
+                    >
                         <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
+                            <button
+                                className={`nav-link ${
+                                    show === "all" && "active"
+                                }`}
+                                type="button"
+                                onClick={() => handleClick("all")}
+                            >
+                                All
+                            </button>
                         </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
+                        {status_list.map((ts, i) => (
+                            <li className="nav-item" key={i}>
+                                <button
+                                    className={`nav-link ${
+                                        show === ts && "active"
+                                    }`}
+                                    type="button"
+                                    onClick={() => handleClick(ts)}
+                                >
+                                    {ts}
+                                </button>
+                            </li>
+                        ))}
                     </ul>
                     <div className="tab-content"></div>
                     <table className="table table-striped ">
                         <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
+                            <tr>
+                                <th scope="col">Name</th>
+                                <th scope="col">Status</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        
+                            {filteredTaskList.length > 0 &&
+                                filteredTaskList.map((task, i) => (
+                                    <tr key={i}>
+                                        <td>{task.task_name}</td>
+                                        <td>{task.task_status}</td>
+                                    </tr>
+                                ))}
                         </tbody>
                     </table>
                 </div>
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,18 +1,276 @@
-import React from 'react';
+import { Button, FormCheck, Modal, Spinner } from "react-bootstrap";
+import React, { useRef, useState } from "react";
+import axios from "axios";
+import DataTable from "./DataTable";
 
+import contactData from "./../data/contactData.json";
+import InfiniteScroll from "react-infinite-scroller";
+import { useNavigate } from "react-router-dom";
+
+const baseUrl = "https://contact.mediusware.com/api/contacts";
 const Problem2 = () => {
+    const scrollParentRef = useRef(null);
+    const navigate = useNavigate();
+    const [showModalA, setShowModalA] = useState(false);
+    const [showModalB, setShowModalB] = useState(false);
+
+    const [allContacts, setAllContacts] = useState(null);
+    const [loadingAllContacts, setLoadingAllContacts] = useState(false);
+    const [usContacts, setUsContacts] = useState(null);
+    const [loadingUsContacts, setLoadingUsContacts] = useState(false);
+
+    const [loadMoreErrorMessage, setLoadMoreErrorMessage] = useState("loading");
+
+    const getAllContacts = () => {
+        setLoadingAllContacts(true);
+        axios
+            .get(`${baseUrl}?format=api&page=0`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*",
+                },
+            })
+            .then((res) => {
+                console.log("res", res);
+                setAllContacts(res.data);
+                setLoadingAllContacts(false);
+            })
+            .catch((err) => {
+                console.log(err);
+                setLoadingAllContacts(false);
+            });
+        setLoadingAllContacts(false);
+        setAllContacts(contactData);
+    };
+
+    const handleCloseModalA = () => {
+        if (showModalA) {
+            setShowModalA(false);
+            navigate("/problem-2");
+        } else {
+            setShowModalA(true);
+            navigate("/problem-2/modal-a");
+            if (usContacts === null) {
+                getAllContacts();
+            }
+        }
+    };
+    const handleCloseModalB = () => {
+        if (showModalB) {
+            setShowModalB(false);
+            navigate("/problem-2");
+        } else {
+            setShowModalB(true);
+            navigate("/problem-2/modal-b");
+        }
+    };
 
     return (
+        <div>
+            {scrollParentRef !== null && (
+                <>
+                    <Modal
+                        show={showModalA}
+                        size="lg"
+                        onHide={handleCloseModalA}
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title>Modal A</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {loadingAllContacts === true && (
+                                <Spinner animation="border" size="sm" />
+                            )}
+                            {loadingAllContacts === false &&
+                                allContacts !== null && (
+                                    <div
+                                        style={{
+                                            maxHeight: "70vh",
+                                            overflowX: "hidden",
+                                            overflowY: "auto",
+                                        }}
+                                        ref={scrollParentRef}
+                                    >
+                                        <InfiniteScroll
+                                            pageStart={0}
+                                            loadMore={() => {
+                                                console.log(
+                                                    "called to fetch more"
+                                                );
+                                                setTimeout(() => {
+                                                    setLoadMoreErrorMessage(
+                                                        "Failed load more due to API CORS error"
+                                                    );
+                                                }, 3000);
+                                            }}
+                                            useWindow={false}
+                                            getScrollParent={() =>
+                                                scrollParentRef.current
+                                            }
+                                            hasMore={true}
+                                            loader={
+                                                <div
+                                                    className="w-100 mt-3 text-center"
+                                                    key={0}
+                                                >
+                                                    {loadMoreErrorMessage ===
+                                                    "loading" ? (
+                                                        <Spinner
+                                                            animation="border"
+                                                            size="sm"
+                                                        />
+                                                    ) : (
+                                                        loadMoreErrorMessage
+                                                    )}
+                                                </div>
+                                            }
+                                        >
+                                            <DataTable
+                                                columns={[
+                                                    "Id",
+                                                    "Phone",
+                                                    "Country",
+                                                ]}
+                                                data={allContacts.results}
+                                            />
+                                        </InfiniteScroll>
+                                    </div>
+                                )}
+                        </Modal.Body>
+                        <Modal.Footer className="justify-content-between">
+                            <FormCheck label="Only Even" />
+                            <div className="d-flex gap-2">
+                                <Button variant="primary" onClick={() => {}}>
+                                    All Contacts
+                                </Button>
+                                <Button variant="warning" onClick={() => {}}>
+                                    US Contacts
+                                </Button>
+                                <Button
+                                    variant="secondary"
+                                    onClick={handleCloseModalA}
+                                >
+                                    Close
+                                </Button>
+                            </div>
+                        </Modal.Footer>
+                    </Modal>
+                    <Modal
+                        show={showModalB}
+                        size="lg"
+                        onHide={handleCloseModalB}
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title>Modal B</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            {loadingAllContacts === true && (
+                                <h3 className="text-center mt-5">
+                                    {loadMoreErrorMessage === "loading" ? (
+                                        <Spinner animation="border" size="sm" />
+                                    ) : (
+                                        loadMoreErrorMessage
+                                    )}
+                                </h3>
+                            )}
+                            {loadingAllContacts === false &&
+                                allContacts !== null && (
+                                    <div
+                                        style={{
+                                            maxHeight: "70vh",
+                                            overflowX: "hidden",
+                                            overflowY: "auto",
+                                        }}
+                                    >
+                                        <InfiniteScroll
+                                            pageStart={0}
+                                            loadMore={() => {
+                                                console.log(
+                                                    "called to fetch more"
+                                                );
+                                                setTimeout(() => {
+                                                    setLoadMoreErrorMessage(
+                                                        "Failed load more due to API CORS error"
+                                                    );
+                                                }, 3000);
+                                            }}
+                                            useWindow={false}
+                                            getScrollParent={() =>
+                                                scrollParentRef.current
+                                            }
+                                            hasMore={true}
+                                            loader={
+                                                <div
+                                                    className="w-100 mt-3 text-center"
+                                                    key={0}
+                                                >
+                                                    {loadMoreErrorMessage ===
+                                                    "loading" ? (
+                                                        <Spinner
+                                                            animation="border"
+                                                            size="sm"
+                                                        />
+                                                    ) : (
+                                                        loadMoreErrorMessage
+                                                    )}
+                                                </div>
+                                            }
+                                        >
+                                            <DataTable
+                                                columns={[
+                                                    "Id",
+                                                    "Phone",
+                                                    "Country",
+                                                ]}
+                                                data={allContacts.results}
+                                            />
+                                        </InfiniteScroll>
+                                    </div>
+                                )}
+                        </Modal.Body>
+                        <Modal.Footer className="justify-content-between">
+                            <FormCheck label="Only Even" />
+                            <div className="d-flex gap-2">
+                                <Button variant="primary" onClick={() => {}}>
+                                    All Contacts
+                                </Button>
+                                <Button variant="warning" onClick={() => {}}>
+                                    US Contacts
+                                </Button>
+                                <Button
+                                    variant="secondary"
+                                    onClick={handleCloseModalB}
+                                >
+                                    Close
+                                </Button>
+                            </div>
+                        </Modal.Footer>
+                    </Modal>
+                </>
+            )}
+            <div className="container">
+                <div className="row justify-content-center mt-5">
+                    <h4 className="text-center text-uppercase mb-5">
+                        Problem-2
+                    </h4>
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
+                    <div className="d-flex justify-content-center gap-3">
+                        <button
+                            className="btn btn-lg btn-outline-primary"
+                            type="button"
+                            onClick={handleCloseModalA}
+                        >
+                            All Contacts
+                        </button>
+                        <button
+                            className="btn btn-lg btn-outline-warning"
+                            type="button"
+                            onClick={handleCloseModalB}
+                        >
+                            US Contacts
+                        </button>
+                    </div>
                 </div>
-                
             </div>
         </div>
     );
